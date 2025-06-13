f6f54db69fdee7774d766e00c45ab7a72e93430d
LogixItsHere <115927522+LogixItsHere@users.noreply.github.com>
2025-06-13 12:22:53 +0700

    init nest js

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+import { User } from './typeorm/entities/User';
+
+const port = Number(process.env.DATABASE_PORT || 5432);
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: port,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [User],
+    synchronize: true,
+
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
